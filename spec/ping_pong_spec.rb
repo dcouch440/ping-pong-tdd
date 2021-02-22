56b782fce71c9d64454f1b83651241b24b6577a7
dcouch440 <dcouch440@gmail.com>
2021-02-22 10:51:17 -0800

    Test => ping_pong => 'pong' Red
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,7 @@ describe("ping_pong") do
   it("It will return ping if the number is divisible by 3") do
     expect(ping_pong(3)).to(eq([1,2,"ping"]))
   end
+  it("It will return pong if the number is divisible by 5") do
+    expect(ping_pong(5)).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
