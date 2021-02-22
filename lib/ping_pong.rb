ca4e3f3801470bfe5f3187a243e5b1da9a92b38f
dcouch440 <dcouch440@gmail.com>
2021-02-22 10:54:51 -0800

    Test => ping_pong => 'pong' PASS
    Co-authored-by: Andy Lawson <alawson89@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,10 +1,12 @@
 def ping_pong(number)
   userArray = Array(1..number)
-  
+
   newArray = userArray.map { |currentIndex|
     if currentIndex % 3 == 0
       "ping"
-    else 
+    elsif currentIndex % 5 == 0
+      "pong"
+    else
       currentIndex
     end
   }
